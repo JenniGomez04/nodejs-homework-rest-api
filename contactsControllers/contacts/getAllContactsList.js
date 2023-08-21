afe64b2e4fdc94c17b3665fd74f03c1fffcfcaed
Jennifer Gómez <jengr42@gmail.com>
2023-08-20 21:12:31 -0500

    Using mongoose and mongoDB_Atlas and jwt.io

--- contactsControllers/contacts/getAllContactsList.js
+++ contactsControllers/contacts/getAllContactsList.js
@@ -1,25 +1,20 @@
-const { Contact } = require("../../schema/index")
+const { Contact } = require("../../schema/index");
 
 const listContacts = async (req, res) => {
-  const { _id: owner } = req.user;
-  try {
-    const contacts = await Contact.find({ owner });
-
-    res.json({
-      status: "success",
-      code: 200,
-      data: {
-        result: contacts,
-      },
-    });
-  } catch (error) {
-    console.error("Error getting contacts:", error);
-    res.status(500).json({
-      status: "error",
-      code: 500,
-      message: "Internal server error",
-    });
-  }
+  const { _id } = req.user;
+  const { page = 1, limit = 10 } = req.query;
+  const skip = (page - 1) * limit;
+  const contacts = await Contact.find({ owner: _id }, "", {
+    skip,
+    limit: Number(limit),
+  }).populate("owner", "_id name email");
+  res.json({
+    status: "success",
+    code: 200,
+    data: {
+      result: contacts,
+    },
+  });
 };
 
 module.exports = listContacts;
